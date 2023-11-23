a3400a329963cabcbcb993dfd09707bbfe40024e
23-24 <23-24@PC04>
2023-11-23 11:17:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("въведете дължината на правоъгълника");
+            int a =  Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("въведете ширината на правоъгълника");
+            int b = Convert.ToInt32(Console.ReadLine());
+    
+            Console.WriteLine("Лицето на правоъгълника е:");
+            int s = a * b;
+            Console.WriteLine(s);
+        }
+    }
+}
